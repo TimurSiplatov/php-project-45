4bc19054c04221e8c6ec23b16ca2acb7f041929b
Timur Siplatov <jjj_jjj_18@mail.ru>
2022-11-17 10:58:22 +0300

    fix BrainCalc.php

--- src/Games/BrainCalc.php
+++ src/Games/BrainCalc.php
@@ -6,6 +6,20 @@ use function BrainGames\Engine\runGame;
 
 const GAME_DESCRIPTION = 'What is the result of the expression?';
 
+function startCalc()
+{
+    $gameLogic = function () {
+        $operations = ["+", "-", "*"];
+        $firstNumber = rand(0, 20);
+        $secondNumber = rand(0, 20);
+        $operator = $operations[array_rand($operations)];
+        $questionForPlayer = "{$firstNumber} {$operator} {$secondNumber}";
+        $correctAnswer = calculate($operator, $firstNumber, $secondNumber);
+        return [$questionForPlayer, $correctAnswer];
+    };
+    runGame(GAME_DESCRIPTION, $gameLogic);
+}
+
 function calculate(string $operator, int $firstNumber, int $secondNumber): string
 {
     switch ($operator) {
@@ -20,18 +34,4 @@ function calculate(string $operator, int $firstNumber, int $secondNumber): strin
             break;
     }
     return (string) $correctAnswer;
-}
-
-function startCalc()
-{
-    $gameLogic = function () {
-        $operations = ["+", "-", "*"];
-        $firstNumber = rand(0, 20);
-        $secondNumber = rand(0, 20);
-        $operator = $operations[array_rand($operations)];
-        $questionForPlayer = "{$firstNumber} {$operator} {$secondNumber}";
-        $correctAnswer = calculate($operator, $firstNumber, $secondNumber);
-        return [$questionForPlayer, $correctAnswer];
-    };
-    runGame(GAME_DESCRIPTION, $gameLogic);
 }
